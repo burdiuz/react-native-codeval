0266baffcb266f080a8dce75dbc885865e06cf33
Oleg Galaburda <burdiuz@gmail.com>
2019-06-23 09:00:09 +0300

    fix webpack-like modules support that return exports from IIFE

--- source/evaluate.js
+++ source/evaluate.js
@@ -1,9 +1,15 @@
 const createEnvWrapper = (globals) => `{${Object.keys(globals).join(', ')}}`;
 
 export const generateEnvironment = (globals) => {
-  const wrapper = eval(`(${createEnvWrapper(globals)}) => (__$__code__, exports = {}) => {
+  const wrapper = eval(`(${createEnvWrapper(globals)}) => (__$__code__, __$__exports__ = {}) => {
+  const exports = __$__exports__;
   const module = { exports };
-  eval(__$__code__);
+  const __$__eval_result__ = eval(__$__code__);
+
+  if(__$__eval_result__ && module.exports === __$__exports__ && !Object.keys(module.exports).length) {
+    return __$__eval_result__;
+  }
+
   return module.exports;
 }`);
 
