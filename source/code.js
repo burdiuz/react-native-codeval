47f21901974b2bc58b3db5c7dcadc7201a4d550c
Oleg Galaburda <burdiuz@gmail.com>
2019-11-19 00:02:26 +0200

    filter out dupes from list of dependencies

--- source/code.js
+++ source/code.js
@@ -1,9 +1,19 @@
-  const CODE_SEARCH_RGX = /require\s*\(\s*["'`][^"'`]+/g;
-  const REQUIRE_NAME_RGX = /[^"'`]+$/;
+const CODE_SEARCH_RGX = /(^|[^\w.])require\s*\(\s*["'`][^"'`]+/g;
+const REQUIRE_NAME_RGX = /[^"'`]+$/;
 
 export const findRequiredDependencyNames = (code) => {
   // env supports <= groups
   // return code.match(/(?<=require\s*\(\s*["'`])([^"'`]+)/g);
 
-  return (code.match(CODE_SEARCH_RGX) || []).map((req) => req.match(REQUIRE_NAME_RGX)[0]);
+  const names = {};
+  return (code.match(CODE_SEARCH_RGX) || [])
+    .map((req) => req.match(REQUIRE_NAME_RGX)[0])
+    .filter((item) => {
+      if (names[item]) {
+        return false;
+      }
+
+      names[item] = true;
+      return true;
+    });
 };
