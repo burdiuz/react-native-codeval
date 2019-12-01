2bfe8a7fe516bcf776acf1a0545df3ef3860d7e4
Oleg Galaburda <burdiuz@gmail.com>
2019-12-02 00:48:01 +0200

    add global.performance mock to prevent an Illegal Operation error in debug

--- source/globals.js
+++ source/globals.js
@@ -7,6 +7,13 @@ function GlobalMock() {
   Object.defineProperties(this, {
     global: { value: this, enumerable: true, configurable: false },
     self: { value: this, enumerable: true, configurable: false },
+    /* 
+      Requesting global.performance in a debug mode will cause TypeError: Illegal invocation, 
+      so I have to restrict access to it via prototype.
+
+      Prettier requests it.
+     */
+    performance: { value: undefined },
   });
 }
 
